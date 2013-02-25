239b59dcd2ed4b24eacfd0ff99e2e6d091b7eeac
Clovis Gladstone <clovis@pantagruel.ci.uchicago.edu>
2013-02-25 16:31:38 -0600

    add missing file

---
+++ www/functions/clean_hitlists.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import os
+import re
+import datetime
+from random import randint
+
+def clean_hitlists():
+    if randint(0,1000) == 0:
+        path = re.sub(r'reports$', '', os.getcwd()) + '/hitlists/'
+        for filename in [path + i for i in os.listdir(path)]:
+            file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+            if datetime.datetime.now() - file_modified > datetime.timedelta(hours=1):
+                os.remove(filename)
+        return True
+    else:
+        return False
+    
