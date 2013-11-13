9f497057fe6f8ce91ee658fd37f88959c774f4d0
Clovis Gladstone <clovis@pantagruel.ci.uchicago.edu>
2013-11-13 10:15:49 -0600

    clean up navigation code

--- www/reports/__init__.py
+++ www/reports/__init__.py
@@ -4,7 +4,7 @@ from kwic import kwic
 from collocation import collocation, fetch_collocation, link_to_concordance
 from relevance import relevance
 from landing_page import landing_page
-from navigation import navigation, get_neighboring_pages, navigate_doc
+from navigation import navigation, navigate_doc
 from concordance_from_collocation import concordance_from_collocation
 from time_series import time_series
 from error import error
