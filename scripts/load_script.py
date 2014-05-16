67a8ed3817a736e4ef2ac6b06efe87ae5825c650
Clovis Gladstone <clovisgladstone@gmail.com>
2014-05-16 09:17:45 -0500

    Update load_script.py

--- scripts/load_script.py
+++ scripts/load_script.py
@@ -128,7 +128,7 @@ l = Loader(data_destination,
 
 l.add_files(files)
 filenames = l.list_files()
-## The following line create a list of the files to parse and sorts the files by filename
+## The following line creates a list of the files to parse and sorts the files by filename
 ## Should you need to supply a custom sort order from the command line you need to supply the files variable,
 ## defined at the top of this script, instead of filenames, like so: 
 ## load_metadata = [{"filename":f} for f in files] 
