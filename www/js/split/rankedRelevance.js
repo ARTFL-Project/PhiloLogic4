a70788d0eb775fbdf85be5616369374cf2303c50
Clovis <clovisgladstone@gmail.com>
2015-02-02 17:23:45 -0600

    remove files

--- www/js/split/rankedRelevance.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var pathname = window.location.pathname.replace('dispatcher.py/', '');
-    var db_url = db_locals['db_url'];
-    var q_string = window.location.search.substr(1);
-    $('.sidebar_display').css('margin-top', '10px');
-    display_options_on_selected();
-    sidebar_reports(q_string, db_url, pathname);
-});
