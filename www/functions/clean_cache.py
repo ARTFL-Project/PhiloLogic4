d9575db42c44576e814e48151452e7deba6e2a72
clovis <clovisgladstone@gmail.com>
2016-04-10 20:17:09 -0500

    remove useless returns

--- www/functions/clean_cache.py
+++ www/functions/clean_cache.py
@@ -4,13 +4,11 @@ import os
 import datetime
 from random import randint
 
+
 def clean_hitlists():
-    if randint(0,1000) == 0:
+    if randint(0, 1000) == 0:
         path = os.path.abspath(os.path.dirname(__file__)).replace('functions', '') + '/data/hitlists/'
         for filename in [path + i for i in os.listdir(path)]:
             file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
             if datetime.datetime.now() - file_modified > datetime.timedelta(hours=1):
                 os.remove(filename)
-        return True
-    else:
-        return False
