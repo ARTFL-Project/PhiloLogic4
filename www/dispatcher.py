dfe45c49dffd6c4fad099789ebce59bf2f0df25d
Clovis <clovisgladstone@gmail.com>
2016-06-16 09:27:59 -0500

    add local copies of external CSS/JS dependencies

--- www/dispatcher.py
+++ www/dispatcher.py
@@ -7,19 +7,19 @@ from cgi import FieldStorage
 from random import randint
 from wsgiref.handlers import CGIHandler
 
-from philologic.DB import DB
+from philologic.app import WebConfig, WSGIHandler
 
 import reports
 from webApp import angular
-from philologic.app import WSGIHandler
-from philologic.app import WebConfig
+
+path = os.path.abspath(os.path.dirname(__file__))
 
 
 def philo_dispatcher(environ, start_response):
     """Dispatcher function."""
-    config = WebConfig(os.path.abspath(os.path.dirname(__file__)))
+    config = WebConfig(path)
     request = WSGIHandler(environ, config)
-    if request.content_type == "application/json":
+    if request.content_type == "application/json" or request.format == "json":
         try:
             path_components = [c for c in environ["PATH_INFO"].split("/") if c]
         except:
@@ -28,19 +28,20 @@ def philo_dispatcher(environ, start_response):
             if path_components[-1] == "table-of-contents":
                 yield ''.join([i for i in reports.table_of_contents(environ, start_response)])
             else:
-                yield ''.join([i for i in getattr(reports, "navigation")(environ, start_response)])
+                yield ''.join([i for i in reports.navigation(environ, start_response)])
         else:
-            report = FieldStorage().getvalue('report')
-            yield ''.join([i for i in getattr(reports, report or "concordance")(environ, start_response)])
+            report = getattr(reports, FieldStorage().getvalue('report'))
+            yield ''.join([i for i in report(environ, start_response)])
     else:
         yield angular(environ, start_response)
 
 
 if __name__ == "__main__":
     CGIHandler().run(philo_dispatcher)
+    # clean-up hitlist every now and then
     if randint(0, 1000) == 0:
-        path = os.path.abspath(os.path.dirname(__file__)) + '/data/hitlists/'
-        for filename in [path + i for i in os.listdir(path)]:
+        hit_list_path = os.path.join(path, '/data/hitlists/')
+        for filename in [os.path.join(hit_list_path, i) for i in os.listdir(hit_list_path)]:
             file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
             if datetime.datetime.now() - file_modified > datetime.timedelta(hours=1):
                 os.remove(filename)
