10ede0607aef888353a7107312246de8ee3d4c5e
Clovis <clovisgladstone@gmail.com>
2017-01-06 14:56:10 -0600

    remove title prefix removal

--- python/philologic/Config.py
+++ python/philologic/Config.py
@@ -599,16 +599,6 @@ web_config_defaults = {
         'comment': '# The time_series_interval variable defines the default year span used for time series.',
         'index': 32
     },
-    'title_prefix_removal': {
-        'value': [],
-        'comment': '''
-                 # The title_prefix_removal variable is only used for displaying titles in a proper order in the landing page browsing function.
-                 # It is used to ignore predefined words at the beginning of a title for sorting purposes.
-                 # e.g: ["the ", "a "] will ignore "the " and "a " for sorting in titles such as "The First Part of King Henry the Fourth", or "A Midsummer Night's Dream".
-                 # Don't forget to add a space after your prefix or the prefix will match partial words.
-                 ''',
-        'index': 33
-    },
     'page_images_url_root': {
         'value': '',
         'comment': '''
