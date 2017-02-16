aa040c8386d13915d39267879feb8333712a3e61
Clovis <clovisgladstone@gmail.com>
2017-02-16 10:52:19 -0600

    faster sqlite access in generate words and ids script

--- extras/generate_words_and_ids.py
+++ extras/generate_words_and_ids.py
@@ -7,6 +7,7 @@ import sys
 
 from philologic.DB import DB
 
+
 def main(db_path):
     """Grab words from words table and dump to file"""
     philo_db = DB(db_path)
@@ -20,19 +21,27 @@ def main(db_path):
     cursor.execute('SELECT philo_id from toms where philo_type="doc" order by rowid')
     docs = [i["philo_id"].split()[0] for i in cursor.fetchall()]
 
+    print("Processing %d documents" % len(docs))
     for doc_id in docs:
         words = []
-        query = "{} %".format(doc_id)
-        cursor.execute("SELECT philo_name, philo_id, philo_type from words where philo_id like ? order by rowid", (query,))
-        for word, philo_id, philo_type in cursor.fetchall():
-            if philo_type == "word" and word != "philo_virtual":
-                words.append(dumps({"token": word, "position": philo_id}))
+        current_doc_id = "{} %".format(doc_id)
+        cursor.execute("SELECT rowid from words where philo_id like ? order by rowid limit 1", (current_doc_id,))
+        first_rowid = cursor.fetchone()[0]
+        next_doc_id = "{} %".format(int(doc_id)+1)
+        cursor.execute("SELECT rowid from words where philo_id like ? order by rowid limit 1", (next_doc_id,))
+        try:
+            last_rowid = cursor.fetchone()[0]
+            cursor.execute("SELECT philo_name, philo_id from words where rowid >= ? and rowid < ? and philo_type=? and philo_type!=? order by rowid",
+                           (first_rowid, last_rowid, "word", "__philo_virtual"))
+        except TypeError:  # we've reached the end of the table and cursor returned None
+            cursor.execute("SELECT philo_name, philo_id from words where rowid >= ? and philo_type=? and philo_type!=? order by rowid",
+                           (first_rowid, "word", "__philo_virtual"))
+        for word, philo_id in cursor.fetchall():
+            words.append({"token": word, "position": philo_id})
         with open(os.path.join(words_and_ids_path, doc_id), "w") as output:
-            output.write("\n".join(words))
+            output.write("\n".join(dumps(w) for w in words))
         print("Processed document %s" % doc_id)
-
 
 if __name__ == '__main__':
     DB_PATH = os.path.join(sys.argv[1], "data")
     main(DB_PATH)
-
