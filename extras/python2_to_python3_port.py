1cc5e6f805f6257f66a83a7635a86d4ccf0eeae1
Clovis Gladstone <clovisgladstone@gmail.com>
2019-02-19 11:14:14 -0600

    clean-up

--- extras/python2_to_python3_port.py
+++ extras/python2_to_python3_port.py
@@ -13,7 +13,6 @@ TWO_TO_THREE_EXEC = "2to3-2.7"
 
 def convert_config(database_to_convert, config_file):
     """Convert config files"""
-    # os.system(f"mv {database_to_convert}/data/{config_file} {database_to_convert}/data/{config_file}_python2")
     os.system(f"{TWO_TO_THREE_EXEC} -w {database_to_convert}/data/{config_file}")
     os.system(f"black -l 120 {database_to_convert}/data/{config_file}")
 
