369be867a5d578203a24d18037695b8d8d246bbf
Clovis Gladstone <clovisgladstone@gmail.com>
2020-02-19 09:27:16 -0600

    make get_frequency take a custom function

--- www/scripts/get_frequency.py
+++ www/scripts/get_frequency.py
@@ -4,33 +4,37 @@ import json
 import os
 from wsgiref.handlers import CGIHandler
 
-from philologic.runtime import frequency_results
-
 import sys
+
 sys.path.append("..")
 import custom_functions
+
 try:
-     from custom_functions import WebConfig
+    from custom_functions import WebConfig
 except ImportError:
-     from philologic.runtime import WebConfig
+    from philologic.runtime import WebConfig
 try:
-     from custom_functions import WSGIHandler
+    from custom_functions import WSGIHandler
 except ImportError:
-     from philologic.runtime import WSGIHandler
+    from philologic.runtime import WSGIHandler
+try:
+    from custom_functions import frequency_results
+except ImportError:
+    from philologic.runtime import frequency_results
 
 
 def get_frequency(environ, start_response):
     """reads through a hitlist. looks up q.frequency_field in each hit, and builds up a list of
        unique values and their frequencies."""
-    status = '200 OK'
-    headers = [('Content-type', 'application/json; charset=UTF-8'),
-               ("Access-Control-Allow-Origin", "*")]
+    status = "200 OK"
+    headers = [("Content-type", "application/json; charset=UTF-8"), ("Access-Control-Allow-Origin", "*")]
     start_response(status, headers)
-    config = WebConfig(os.path.abspath(os.path.dirname(__file__)).replace('scripts', ''))
+    config = WebConfig(os.path.abspath(os.path.dirname(__file__)).replace("scripts", ""))
     request = WSGIHandler(environ, config)
 
     results = frequency_results(request, config)
-    yield json.dumps(results).encode('utf8')
+    yield json.dumps(results).encode("utf8")
+
 
 if __name__ == "__main__":
     CGIHandler().run(get_frequency)
