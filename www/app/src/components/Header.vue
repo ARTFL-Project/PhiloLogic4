a6e6b1c8fa463f06a5dea6e8d74ef2c7a23bc55f
Clovis Gladstone <clovisgladstone@gmail.com>
2022-09-22 10:17:19 -0500

    bug fix to academic citation

--- www/app/src/components/Header.vue
+++ www/app/src/components/Header.vue
@@ -59,7 +59,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">How to cite {{ philoConfig.dbname }}</h5>
+                        <h5 class="modal-title">How to cite {{ philoConfig.dbname }}:</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
@@ -86,7 +86,7 @@ export default {
         return {
             philoConfig: this.$philoConfig,
             date: this.getDate(),
-            docCitation: {},
+            docCitation: { citation: [] },
         };
     },
     created() {
